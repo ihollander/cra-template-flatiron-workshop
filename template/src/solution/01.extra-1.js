a63c21191da2cae6a96833683e43fe854b499619
ihollander <ianholla@gmail.com>
2021-04-20 09:32:17 -0400

    Initial commit

---
+++ template/src/solution/01.extra-1.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+
+function Clock() {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return function cleanup() {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  return <div>{time.toString()}</div>;
+}
+
+function App() {
+  const [isShowingClock, setIsShowingClock] = useState(true);
+
+  function toggle() {
+    setIsShowingClock(isShowingClock => !isShowingClock);
+  }
+
+  return (
+    <div>
+      {isShowingClock ? <Clock /> : null}
+      <button onClick={toggle}>{isShowingClock ? "Hide" : "Show"} Clock</button>
+    </div>
+  );
+}
+
+export default App;
